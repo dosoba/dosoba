24afb53f6b798355bab0a05ef2c2dd4f7978077c
joongs <rlatpwnd8314@naver.com>
2014-11-04 23:45:01 +0900

    Update linkedList.h

--- header/linkedList.h
+++ header/linkedList.h
@@ -1,13 +1,53 @@
 #ifndef __LINKEDLIST__
 #define __LINKEDLIST__
 
-void createLinkedList(LINKEDLIST *self) ;
+#define USE 1
+#define NOT_USE 0
+
+#define TRUE 1
+#define FALSE 0
+
+typedef struct ST_NODE NODE;
+typedef struct ST_PERSONALINFO PERSONALINFO;
+typedef struct ST_LINKEDLIST LINKEDLIST;
+
+typedef struct ST_NODE {
+	PERSONALINFO *info;
+	NODE *next;
+} NODE;
+
+typedef struct ST_LINKEDLIST {
+	NODE *head;
+	NODE *tail;
+	NODE *pos;
+	int length;
+
+	void (*createLinkedList)(LINKEDLIST*);
+	void (*destroyLinkedList)(LINKEDLIST*);
+	int (*findNameLinkedList)(LINKEDLIST*, char*);
+	void (*appendLinkedList)(LINKEDLIST*, PERSONALINFO*);
+	void (*insertLinkedList)(LINKEDLIST*, int, PERSONALINFO*);
+	void (*deleteLinkedList)(LINKEDLIST*, int);
+	void (*deleteAllLinkedList)(LINKEDLIST*);
+	NODE* (*moveFirstLinkedList)(LINKEDLIST*);
+	NODE* (*moveLastLinkedList)(LINKEDLIST*);
+	NODE* (*nextLinkedList)(LINKEDLIST*);
+	NODE* (*moveToBeforeNodeLinkedList)(LINKEDLIST*, int);
+	PERSONALINFO (*viewAtLinkedList)(LINKEDLIST*, int);
+	int (*isTailLinkedList)(LINKEDLIST*);
+	int (*getLengthLinkedList)(LINKEDLIST*);
+	NODE* (*getNodeLinkedList)(LINKEDLIST*, int);
+	void (*printHeaderLinkedList)(int);
+	void (*printPersonInfoLinkedList)(PERSONALINFO*, int);
+	void (*printLinedList)(LINKEDLIST*);
+} LINKEDLIST;
+
+void createLinkedList(LINKEDLIST *self);
 void destroyLinkedList(LINKEDLIST *self);
 
 int findNameLinkedList(LINKEDLIST *self, char* p_name);
-
 void appendLinkedList(LINKEDLIST *self, PERSONALINFO* p_info);
-void insertLinkedList(LINKEDLIST *self, int index, PERSONALINFO* p_info) ;
+void insertLinkedList(LINKEDLIST *self, int index, PERSONALINFO* p_info);
 void deleteLinkedList(LINKEDLIST *self, int index);
 void deleteAllLinkedList(LINKEDLIST *self);
 
