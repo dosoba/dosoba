7f78cd6bf691d002fed8e5a60f58c56e8e57124f
joongs <rlatpwnd8314@naver.com>
2014-11-23 14:04:51 +0900

    Create personinfo.h

---
+++ 20141123_CLASS_of_addressBook_by_Cplus/personinfo.h
@@ -0,0 +1,21 @@
+/*********************
+* Name : personinfo.h
+* Date : 2014.11.16
+* Writer : Sejoong Kim
+**********************/
+
+#ifndef PERSONINFO_H_
+#define PERSONINFO_H_
+
+#include <string>
+
+using namespace std;
+
+class PersonalInfo {
+public:
+	string name;
+	string phone;
+	string address;
+};
+
+#endif /* PERSONINFO_H_ */
