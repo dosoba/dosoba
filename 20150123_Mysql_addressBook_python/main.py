89315d5a8564c3939aa93fe5aedb460ccb82531d
joongs <rlatpwnd8314@naver.com>
2015-01-25 22:47:51 +0900

    Mysql #1: init
    1. Mysql 디렉토리 생성
    2. python 코드 복사

---
+++ 20150123_Mysql_addressBook_python/main.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+
+"""
+Name : main.cpp
+Date : 2015.01.23
+Writer : Sejoong Kim
+"""
+
+import addressBook
+
+def main():
+    menu = -1
+    book = addressBook.addressbook()
+
+    book.testSetup()
+
+    while (menu != 0):
+        book.printMainMenu()
+
+        menu = raw_input("menu: ")
+
+        menu = int(menu)
+
+        if menu == book.MENU_INPUT:
+            book.input()
+        elif menu == book.MENU_REMOVE:
+            book.remove()
+        elif menu == book.MENU_MODIFY:
+            book.modify()
+        elif menu == book.MENU_SEARCH:
+            book.search()
+        elif menu == book.MENU_ALL_PRINT:
+            book.printAll()
+        elif menu == book.MENU_SAVE:
+            book.save()
+        elif menu == book.MENU_LOAD:
+            book.load()
+        elif menu == book.MENU_FINISH:
+            book.print_finish()
+        else:
+            book.print_error(book.ERROR_NO_MENU)
+
+        if (menu != book.MENU_FINISH):
+            raw_input("menu 화면으로 돌아갑니다. (아무키나 누르세요)")
+
+if __name__ == "__main__":
+    main()
