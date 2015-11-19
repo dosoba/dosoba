a47df8e9176ff3d3e87dd248ed6f0dbe05a37344
joongs <rlatpwnd8314@naver.com>
2015-11-19 22:03:44 +0900

    1.2. 파일입출력
    문제에서 주어지는 파일 입출력에 대해서 구현되 코드 이해

--- 20151118_Algorithm_To_ad/chapter1/chapter1/main.cpp
+++ 20151118_Algorithm_To_ad/chapter1/chapter1/main.cpp
@@ -1,32 +1,63 @@
+/**********************************
+* 제목 : 파일 입출력
+* 페이지 : 14 ~ 17
+* 코드 : c / c++
+* Q : 다음 내용을 표준 입력으로 읽어 들여 변수에 저장 후 출력하시오.
+* 주의점 : c로 코드 작성시 scanf(" %c", &ary[i][j]);
+**********************************/
 
-//Q : 다음 내용을 표준 입력으로 읽어 들여 변수에 저장 후 출력하시오.
-// c++
-#include <iostream>
-using namespace std;
-
-int main(int argc, char argv[]) {
-	char input;
-
-	input = getchar();
-	while (input != EOF) {
-		cout << input;
-		input = getchar();
-	}
-	
-	return 0;
-}
-
-
-//c
+// c
 #include <stdio.h>
 
 int main(int argc, char argv[]) {
-	char input;
 
-	input = getchar();
-	while (input != EOF) {
-		printf("%d", input);
-		input = getchar();
+	int T, r, c;
+	char ary[100][100];
+
+	scanf("%d", &T);
+	scanf("%d %d", &r, &c);
+
+	for (int i = 0; i < r; i++) {
+		for (int j = 0; j < c; j++) {
+			scanf(" %c", &ary[i][j]);
+		}
+	}
+
+	printf("%d\n%d %d\n", T, r, c);
+
+	for (int i = 0; i < r; i++) {
+		for (int j = 0; j < c; j++) {
+			printf("%c", ary[i][j]);
+		}
+		printf("\n");
+	}
+
+	return 0;
+}
+
+// c++
+#include <iostream>
+using namespace std;
+int main(int argc, char argv[]) {
+
+	int T, r, c;
+	char ary[100][100];
+
+	cin >> T >> r >> c;
+
+	for (int i = 0; i < r; i++) {
+		for (int j = 0; j < c; j++) {
+			cin >> ary[i][j];
+		}
+	}
+
+	cout << T << endl << r << " " << c << endl;
+
+	for (int i = 0; i < r; i++) {
+		for (int j = 0; j < c; j++) {
+			cout << ary[i][j];
+		}
+		cout << endl;
 	}
 
 	return 0;
