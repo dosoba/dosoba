ac542484d07605f901dcb19f19f38199e58e6d67
joongs <rlatpwnd8314@naver.com>
2015-11-19 21:20:59 +0900

    15page - 표준입출력
    문제 제시 : 다음 내용을 표준 입력으로 읽어 들여 변수에 저장 후 출력하시오.
    구현 : c++ / c 로 구현
    파일 : input.txt / output.txt / main.cpp

--- 20151118_Algorithm_To_ad/chapter1/chapter1/main.cpp
+++ 20151118_Algorithm_To_ad/chapter1/chapter1/main.cpp
@@ -1,5 +1,33 @@
+
+//Q : 다음 내용을 표준 입력으로 읽어 들여 변수에 저장 후 출력하시오.
+// c++
 #include <iostream>
+using namespace std;
 
 int main(int argc, char argv[]) {
+	char input;
+
+	input = getchar();
+	while (input != EOF) {
+		cout << input;
+		input = getchar();
+	}
+	
+	return 0;
+}
+
+
+//c
+#include <stdio.h>
+
+int main(int argc, char argv[]) {
+	char input;
+
+	input = getchar();
+	while (input != EOF) {
+		printf("%d", input);
+		input = getchar();
+	}
+
 	return 0;
 }
