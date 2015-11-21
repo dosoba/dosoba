3df179da5807b0b346cc48e5f7196dedb010e7e7
joongs <rlatpwnd8314@naver.com>
2015-11-21 17:29:02 +0900

    비트 - 연습문제 1(다시 풀이)
    1. 자리수에 따른 부분 수정
    2. 방식 변경

--- 20151118_Algorithm_To_ad/chapter1/chapter1/bit_practice1.cpp
+++ 20151118_Algorithm_To_ad/chapter1/chapter1/bit_practice1.cpp
@@ -2,55 +2,71 @@
 * 제목 : 비트 연습문제 1  - 0과 1로 이루어진 1차 배열에서 7개 byte를 묶어서 10진수로 출력하기
 * 페이지 : 19
 * 코드 : c
+* 작성 : 2015-11-21
 **********************************/
+
 #include <stdio.h>
 
-#define POSITION_NUMBER 7
-#define MAX 70
+#define NUMBER 7
 
-void ConvertFromBinaryIntoDecimal(char *(binaryNumbers));
+void BinaryToDecimalPrint(int *(array), int size);
 
 int main(int argc, char argv[]) {
 
-	char input;
-	char binaryNumbers[MAX];
-	int i;
+	//int array[] = { 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 0, 1 };
+	int array[] = {0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1};
+	int size;
 
-	i = 0;
-	input = getchar();
-	while (input != EOF) {
-		binaryNumbers[i] = input;
-		input = getchar();
-		i++;
-	}
+	size = sizeof(array) / sizeof(int);
 
-	ConvertFromBinaryIntoDecimal(binaryNumbers);
-
+	BinaryToDecimalPrint(array, size);
 	return 0;
 }
 
-void ConvertFromBinaryIntoDecimal(char *(binaryNumbers)) {
-	int result;
-	int i;
+void BinaryToDecimalPrint(int *(array), int size) {
+	int decimal;
+	int start;
+	int end;
+	int i = 1;
 	int j;
+	int repeat;
 
-	j = 0;
-	while (binaryNumbers[j*POSITION_NUMBER] == '0' || binaryNumbers[j*POSITION_NUMBER] == '1') {
-		i = 0;
-		result = 0;
-		while (i < POSITION_NUMBER) {
-			if (binaryNumbers[j*POSITION_NUMBER + i] == '1') {
-				result += (1 << (POSITION_NUMBER - 1 - i));
-			}
-			i++;
-		}
-		printf("%d ", result);
-		j++;
+	if (size%NUMBER != 0) {
+		repeat = (size / NUMBER) + 1;
 	}
+	else {
+		repeat = (size / NUMBER);
+	}
+
+
+	while (i <= repeat) {
+
+		start = (NUMBER) * i - 1;
+		if (start > size) {
+		start = size;
+		}
+
+		end = NUMBER * (i - 1);
+		decimal = 0;
+
+		j = start;
+		while (j >= end && start != end) {
+			if (array[j] == 1) {
+				decimal += (1 << (start - j));
+			}
+			j--;
+		}
+		printf("%d ", decimal);
+
+		i++;
+	}
+
 }
 
-//결과
-//입력
-00000010001101
-//출력
-1 13
+// 결과
+input  : 0000000111100000011000000111100110000110000111100111100111111001100111
+output : 0 120 12 7 76 24 60 121 124 103
+
+input  : 00000010001101
+output : 1 13
+
