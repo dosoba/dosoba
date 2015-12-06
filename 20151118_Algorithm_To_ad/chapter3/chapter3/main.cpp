04061b3a3351b930e5ab82d22c66a1d448ad0fef
joongs <rlatpwnd8314@naver.com>
2015-12-06 20:46:19 +0900

    3.0. 분할정복 백트레킹 준비
    준비파일 업로드

---
+++ 20151118_Algorithm_To_ad/chapter3/chapter3/main.cpp
@@ -0,0 +1,23 @@
+/**********************************
+* 力格 :
+* 其捞瘤 :
+* 内靛 : c
+**********************************/
+
+#include <stdio.h>
+
+int main(int argc, char argv[]) {
+
+	int T;
+	int inputNumber;
+
+	scanf("%d", &T);
+
+	for (int test_case = 1; test_case <= T; test_case++) {
+		scanf("%d ", &inputNumber);
+
+		printf("#%d\n", test_case);
+	}
+
+	return 0;
+}
