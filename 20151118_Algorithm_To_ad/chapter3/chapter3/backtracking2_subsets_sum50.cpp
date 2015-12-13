b5bcb78e387d6f4f507431ae0137af093b8c4fdc
joongs <rlatpwnd8314@naver.com>
2015-12-13 15:54:24 +0900

    3.2. 백트래킹 기본 - 2
    /**********************************
    * 제목 : 부분집합의 합이 50일 경우만 출력
    * 페이지 : 참고 사이트 참조
    * 코드 : c
    **********************************/

---
+++ 20151118_Algorithm_To_ad/chapter3/chapter3/backtracking2_subsets_sum50.cpp
@@ -0,0 +1,57 @@
+/**********************************
+* 제목 : 부분집합의 합이 50일 경우만 출력
+* 페이지 : 참고 사이트 참조
+* 코드 : c
+**********************************/
+
+#include <stdio.h>
+
+#define SIZE 5
+int data[SIZE] = { 0, 40, 20, 30, 10 };
+int include[SIZE];
+
+void Print_arr_except_zero_item();
+void GetSubSetsOfSum50(int index, int total);
+
+int main(int argc, char argv[]) {
+
+	GetSubSetsOfSum50(0, 0);
+
+	return 0;
+}
+
+// 50일 경우만 출력
+void GetSubSetsOfSum50(int index, int total) {
+	if (index == SIZE - 1) {
+		if (total == 50) {
+			Print_arr_except_zero_item();
+		}
+	}
+	else {
+		include[index + 1] = 1;
+		GetSubSetsOfSum50(index + 1, total + data[index + 1]);
+		include[index + 1] = 0;
+		GetSubSetsOfSum50(index + 1, total);
+	}
+}
+
+//출력시 0이 부부은 제외하고 출력한다.
+void Print_arr_except_zero_item() {
+	int i = 1;
+
+	while (i < SIZE) {
+		if (include[i] == 1) {
+			printf("%d ", data[i]);
+		}
+		i++;
+	}
+
+	putchar('\n');
+}
+
+//input
+none
+
+//output
+40 10
+20 30
