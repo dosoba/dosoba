7977b22b73bb10612ec0755232a997c682ae30b7
joongs <rlatpwnd8314@naver.com>
2016-01-29 20:48:44 +0900

    chapter6 DP1 초기세업
    초기셋업

---
+++ 20151118_Algorithm_To_ad/chapter6/chapter6/main.c
@@ -0,0 +1,19 @@
+/**********************************
+* 力格 : 
+* 其捞瘤 :
+* 内靛 : c
+**********************************/
+
+#include <stdio.h>
+
+int main(int argc, char argv[]) {
+	int T;
+
+	scanf("%d ", &T);
+	for (int test_case = 1; test_case <= T; test_case++) {
+
+		printf("#%d ", test_case);
+	}
+
+	return 0;
+}
