8c1d82eb97f51dce9ccb452ee1d44008740de834
joongs <rlatpwnd8314@naver.com>
2016-02-21 20:37:18 +0900

    3.7. 순열(사전순X) VS 순열(사전순O)
    순열(사전순X) VS 순열(사전순O) 비교

--- 20151118_Algorithm_To_ad/prereview_algorhithm/prereview_algorhithm/3_permutation.cpp
+++ 20151118_Algorithm_To_ad/prereview_algorhithm/prereview_algorhithm/3_permutation.cpp
@@ -1,3 +1,5 @@
+////////////////////////////////////////////////// 사전순이 아닌 순으로 출력하기
+
 /*
 * name : 순열
 * contents : 0, 1, ..., n-1에서 k개를 뽑아 나열하는 경우의 수는 nPk이다. 
@@ -61,4 +63,93 @@ void permutation(int(*arr), int len, int n, int k) {
 		permutation(arr, len + 1, n, k);
 		swap_arr(arr, i, len);
 	}
+}
+
+////////////////////////////////////////////////// 사전순으로 출력하기
+
+/*
+* name : 순열
+* contents : 0, 1, ..., n-1에서 k개를 뽑아 나열하는 경우의 수는 nPk이다.
+n과 k를 입력받아서 모든 경우를 사전순으로 출력하는 프로그램을 작성하시오.
+*/
+
+//input
+3 2
+
+//output
+0 1
+0 2
+1 0
+1 2
+2 0
+2 1
+
+#include <stdio.h>
+#define MAXN 10
+
+void print_arr(int(*set), int set_size);
+void right_rotate(int(*arr), int s, int t);
+void left_rotate(int(*arr), int s, int t);
+void permutation(int(*arr), int len, int n, int k);
+
+int main() {
+
+	int arr[MAXN];
+	int n;
+	int k;
+	scanf(" %d %d", &n, &k);
+	for (int i = 0; i < n; i++) {
+		arr[i] = i;
+	}
+	permutation(arr, 0, n, k);
+	return 0;
+}
+
+void print_arr(int(*set), int set_size) {
+	for (int i = 0; i < set_size; i++) {
+		printf("%d ", set[i]);
+	}
+	printf("\n");
+}
+
+void swap_arr(int(*arr), int i, int j) {
+	int temp;
+	temp = arr[i];
+	arr[i] = arr[j];
+	arr[j] = temp;
+}
+
+void permutation(int(*arr), int len, int n, int k) {
+	int i;
+	if (len == k) {
+		print_arr(arr, len);
+		return;
+	}
+	for (i = len; i < n; i++) {
+		//swap_arr(arr, i, len);
+		right_rotate(arr, len, i);
+		permutation(arr, len + 1, n, k);
+		//swap_arr(arr, i, len);
+		left_rotate(arr, len, i);
+	}
+}
+
+void right_rotate(int(*arr), int s, int t) {
+	int i;
+	int last;
+	last = arr[t];
+	for (int i = t; i > s; i--) {
+		arr[i] = arr[i - 1];
+	}
+	arr[s] = last;
+}
+
+void left_rotate(int(*arr), int s, int t) {
+	int i;
+	int first;
+	first = arr[s];
+	for (i = s; i < t; i++) {
+		arr[i] = arr[i + 1];
+	}
+	arr[t] = first;
 }
