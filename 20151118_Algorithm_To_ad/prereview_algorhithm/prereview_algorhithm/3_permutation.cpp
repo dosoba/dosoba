e765416fe2509dd00f4c600d13e3eb8b284522f3
joongs <rlatpwnd8314@naver.com>
2016-02-21 20:12:19 +0900

    3.7. 순열
    * name : 순열
    * contents : 0, 1, ..., n-1에서 k개를 뽑아 나열하는 경우의 수는 nPk이다.
    n과 k를 입력받아서 모든 경우를 출력한느 프로그램을 작성하시오.

---
+++ 20151118_Algorithm_To_ad/prereview_algorhithm/prereview_algorhithm/3_permutation.cpp
@@ -0,0 +1,64 @@
+/*
+* name : 순열
+* contents : 0, 1, ..., n-1에서 k개를 뽑아 나열하는 경우의 수는 nPk이다. 
+n과 k를 입력받아서 모든 경우를 출력한느 프로그램을 작성하시오.
+*/
+
+//input
+3 2
+
+//output
+0 1
+0 2
+1 0
+1 2
+2 1
+2 0
+
+
+#include <stdio.h>
+#define MAXN 10
+
+void print_arr(int(*set), int set_size);
+void swap_arr(int(*arr), int i, int j);
+void permutation(int(*arr), int len, int n, int k);
+
+int main() {
+
+	int arr[MAXN];
+	int n;
+	int k;
+	scanf(" %d %d", &n, &k);
+	for (int i = 0; i < n; i++) {
+		arr[i] = i;
+	}
+	permutation(arr, 0, n, k);
+	return 0;
+}
+
+void print_arr(int(*set), int set_size) {
+	for (int i = 0; i < set_size; i++) {
+		printf("%d ", set[i]);
+	}
+	printf("\n");
+}
+
+void swap_arr(int(*arr), int i, int j) {
+	int temp;
+	temp = arr[i];
+	arr[i] = arr[j];
+	arr[j] = temp;
+}
+
+void permutation(int(*arr), int len, int n, int k) {
+	int i;
+	if (len == k) {
+		print_arr(arr, len);
+		return;
+	}
+	for (i = len; i < n; i++) {
+		swap_arr(arr, i, len);
+		permutation(arr, len + 1, n, k);
+		swap_arr(arr, i, len);
+	}
+}
