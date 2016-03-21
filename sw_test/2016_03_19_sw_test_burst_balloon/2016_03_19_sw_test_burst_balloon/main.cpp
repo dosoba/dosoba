572c56e0414aaddeb5f26b813f79d4040d5de2a4
joongs <rlatpwnd8314@naver.com>
2016-03-21 23:28:16 +0900

    예제문제 풀이 작성 - x, y 뒤바뀜
    x, y뒤바뀌어서 문제 풀이됨

--- sw_test/2016_03_19_sw_test_burst_balloon/2016_03_19_sw_test_burst_balloon/main.cpp
+++ sw_test/2016_03_19_sw_test_burst_balloon/2016_03_19_sw_test_burst_balloon/main.cpp
@@ -23,7 +23,11 @@ int cntY[MAXN];
 // max
 int max;
 
+// counts
+int map[MAXN][MAXN];
+
 void getMax();
+void print_location(int time);
 
 int main() {
 	int T;
@@ -32,31 +36,45 @@ int main() {
 	scanf("%d", &T);
 	for (test_case = 1; test_case <= T; test_case++) {
 		max = 0;
+
+		//map 초기화
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				map[i][j] = 0;
+			}
+		}
+
 		scanf("%d %d", &N, &K);
 		for (int i = 0; i < K; i++) {
-			scanf("%d %d %d", y[i], x[i], d[i]);
+			scanf("%d %d %d", &y[i], &x[i], &d[i]);
+			map[y[i]][x[i]] += 1;
 		}
 
 		getMax();
 
 		//결과 출력
-		printf("#d %d\n", test_case, max);
+		printf("#%d %d\n", test_case, max);
 	}
 	return 0;
 }
 
 void getMax() {
 	int _max;
-	int maxPerTime;
 	int i;
 	int j;
 	int z;
 	int k;
 	int time;
 
+
 	//0. 100초 동안 지속한다.
 	for (time = 0; time <= 100; time++) {
 		_max = 0;
+
+		//중복체킹 초기화
+		for (k = 0; k < N; k++) {
+			cntY[k] = 0;
+		}
 
 		//충돌한 경우 방향 변경 고려 추가되어야 함
 
@@ -64,34 +82,55 @@ void getMax() {
 			//1. 이동한다, 방향 체크한다.
 			for (i = 0; i < K; i++) {
 				if (d[i] == U) {
+					map[y[i]][x[i]] -= 1;
+					y[i] -= 1;  
+					map[y[i]][x[i]] += 1;
 					if (y[i] == 0) {
 						d[i] = D;
-						y[i] += 1;
 					}
-					else y[i] -= 1;
 				}
-				if (d[i] == D) {
+				else if (d[i] == D) {
+					map[y[i]][x[i]] -= 1;
+					y[i] += 1;
+					map[y[i]][x[i]] += 1;
 					if (y[i] == N - 1) {
 						d[i] = U;
-						y[i] -= 1;
-					}
-					y[i] += 1;
+					};
 				}
-				if (d[i] == L) {
+				else if (d[i] == L) {
+					map[y[i]][x[i]] -= 1;
+					x[i] -= 1;
+					map[y[i]][x[i]] += 1;
 					if (x[i] == 0) {
 						d[i] = R;
-						x[i] += 1;
 					}
-					x[i] -= 1;
 				}
-				if (d[i] == R) {
+				else if (d[i] == R) {
+					map[y[i]][x[i]] -= 1;
+					x[i] += 1;
+					map[y[i]][x[i]] += 1;
 					if (x[i] == N - 1) {
 						d[i] = L;
-						x[i] -= 1;
 					}
-					x[i] += 1;
 				}
 			}
+
+			//2. 중복을 체크한다.
+			for (i = 0; i < N; i++) {
+				for (j = 0; j < N; j++) {
+					if (map[i][j] > 1) {
+						for (k = 0; k < K; k++) {
+							if (i == y[k] && j == x[k]) {
+								if (d[k] == U) d[k] = D;
+								else if (d[k] == D) d[k] = U;
+								else if (d[k] == L) d[k] = R;
+								else if (d[k] == R) d[k] = L;
+							}
+						}
+					}
+				}
+			}
+
 		}
 
 		//3. y가 같은 것을 센다
@@ -111,5 +150,14 @@ void getMax() {
 		if (max < _max) {
 			max = _max;
 		}
+		//print_location(time);
 	}
+}
+
+void print_location(int time) {
+	int i;
+	for (i = 0; i < K; i++) {
+		printf("%d : %d %d %d, ", time, y[i], x[i], d[i]);
+	}
+	printf("\n");
 }
