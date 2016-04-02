75d556ef29fa401a51ebdaaa2b2df2cd1e8a4780
joongs <rlatpwnd8314@naver.com>
2016-04-02 17:07:35 +0900

    0402 sw test 비콘
    비콘문제

---
+++ sw_test/2016_04_02_sw_test_beacon/beacon/beacon/main.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+	int test_case;
+	int T;
+
+	scanf("%d", &T);
+	for (test_case = 1; test_case <= T; test_case++) {
+
+		//input here your solution
+		
+		printf("#%d", test_case);
+	}
+
+	return 0;
+}
