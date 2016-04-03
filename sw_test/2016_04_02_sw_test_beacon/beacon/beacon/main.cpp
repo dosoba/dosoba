f6412674cb66f06bb467346fe361b6b6f6511425
joongs <rlatpwnd8314@naver.com>
2016-04-03 22:48:58 +0900

    update
    update

--- sw_test/2016_04_02_sw_test_beacon/beacon/beacon/main.cpp
+++ sw_test/2016_04_02_sw_test_beacon/beacon/beacon/main.cpp
@@ -1,4 +1,11 @@
 #include <stdio.h>
+#define MAX_TIME 50 + 1
+#define MAX_VISITORS 50 + 1
+
+int beacons[3][2];
+int N;
+//int map[MAX_VISITORS][MAX_TIME];
+
 
 int main() {
 	int test_case;
@@ -8,6 +15,32 @@ int main() {
 	for (test_case = 1; test_case <= T; test_case++) {
 
 		//input here your solution
+
+		//make map 
+		int map[MAX_VISITORS][MAX_TIME] = {0, };
+
+		//get N
+		scanf("%d", &N);
+
+		//get L1, L2, L3
+		for (int i = 0; i < 3; i++) {
+			scanf("%d", &beacons[i][0]);
+		}
+		//get P1, P2, P3
+		for (int i = 0; i < 3; i++) {
+			scanf("%d", &beacons[i][1]);
+		}
+
+		//set data for map
+		for (int i = 1; i <= N; i++) {
+			int A;
+			int D;
+			scanf("%d %d", &A, &D);
+
+			for (int j = A; j < A + D; j++) {
+				map[i][j] = 1;
+			}
+		}
 		
 		printf("#%d", test_case);
 	}
