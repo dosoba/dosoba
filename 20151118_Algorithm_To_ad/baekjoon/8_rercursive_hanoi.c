da6073b2e9c2933150f434fbabe1031d0435fce5
joongs <rlatpwnd8314@naver.com>
2016-05-16 22:20:06 +0900

    Update 8_rercursive_hanoi.c

--- 20151118_Algorithm_To_ad/baekjoon/8_rercursive_hanoi.c
+++ 20151118_Algorithm_To_ad/baekjoon/8_rercursive_hanoi.c
@@ -1,6 +1,10 @@
-// 미해결 문제 - 다시 풀어볼것
-// baekjoon : https://www.acmicpc.net/problem/11729
-// 내용 : https://ko.wikipedia.org/wiki/%ED%95%98%EB%85%B8%EC%9D%B4%EC%9D%98_%ED%83%91
+// baekjoon :https://www.acmicpc.net/problem/11729
+// 내용 : http://terms.naver.com/entry.nhn?docId=2270443&cid=51173&categoryId=51173
+// 내용링크를 통해서 이해할수 있었음
+// 1. 규칙 1 : 왼쪽 기둥에서 2(=n-1)개의 원판을 가운데 기둥으로 옮긴다. 이때 오른쪽 기둥을 이용한다.
+// 2. 왼쪽 기둥의 원판을 오른쪽 기둥으로 옮긴다.
+// 3. 가운데 기둥의 2(=n-1)개의 원판을 오른쪽 기둥으로 옮긴다. 이때 왼쪽 기둥을 이용한다.
+
 
 //input
 3
@@ -16,23 +20,20 @@
 1 3
 
 #include <stdio.h>
-
-void move(int n, int from, int to, int temp) {
-	printf("%d %d %d %d\n", n, from, to, temp);
-	if (n == 0) {
-		printf("n==0\n");
-		return;
-	}
-
-	move(n - 1, from, temp, to);
-	printf("%d %d\n", from, to);
-	move(n - 1, temp, to, from);
-}
+void hanoi(int n, int a, int b, int c);
 
 int main() {
 	int n;
 	scanf("%d", &n);
 	printf("%d\n", (1 << n) - 1);
-	move(n, 1, 3, 2);
+	hanoi(n, 1, 2, 3);
 	return 0;
+}
+
+
+void hanoi(int n, int a, int b, int c) {
+	if (n == 0) return;
+	hanoi(n - 1, a, c, b);
+	printf("%d %d\n", a, c);
+	hanoi(n - 1, b, a, c);
 }
